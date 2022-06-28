df1d57228853be61f1f55fc17767b514166a5bfc
Chris Alex <mujahid.ali@wrld3d.com>
2022-06-28 18:55:06 +0500

    removed indentation

--- cypress/e2e/NFT copy.cy.ts
+++ cypress/e2e/NFT copy.cy.ts
@@ -2,8 +2,9 @@ describe('NFT on open sea api', () => {
   context('GET /NFT', () => {
     it('should show NFT ', () => {
       cy.request({
-        method: 'GET', url: 'https://staging-gateway.wrld.xyz/contracts', headers: { "Cookie": "token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjEsImlhdCI6MTY1Mjc5MDI1NCwiZXhwIjoxNjg0MzI2MjU0fQ.2vIFGELbH54q12-b1SIMwm0Eh7BLmdJMpPEX3E2XYy0" }, failOnStatusCode: false }).then(
-       (response)  => {
+        method: 'GET', url: 'https://staging-gateway.wrld.xyz/contracts', headers: { "Cookie": "token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjEsImlhdCI6MTY1Mjc5MDI1NCwiZXhwIjoxNjg0MzI2MjU0fQ.2vIFGELbH54q12-b1SIMwm0Eh7BLmdJMpPEX3E2XYy0" }, failOnStatusCode: false
+      }).then(
+        (response) => {
           expect(response.body.data[1].contractAbi[0].inputs[0]).to.have.property('internalType').contains('uint256')
           //OR
           expect(response.body.data[1].contractAbi[0].inputs[0].internalType).contains('uint256')
@@ -11,8 +12,8 @@ describe('NFT on open sea api', () => {
           expect(response.body.data[1].contractAbi[0].inputs[0].name).contain('_mintSupplyCount')
           expect(response.body.data[1].contractAbi[0].inputs[0].type).contain('uint256')
           expect(response.body.data[1].contractAbi[0].inputs[1].name).contain('_ownerMintReserveCount');
-          expect(response.status).to.eq(200); 
+          expect(response.status).to.eq(200);
         })
-  });
+    });
   })
 })
