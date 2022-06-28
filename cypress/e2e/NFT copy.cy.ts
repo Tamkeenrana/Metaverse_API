2dc2fdee2eabd1a3fadd81b058c92fb8284fe5c9
Chris Alex <mujahid.ali@wrld3d.com>
2022-06-28 19:03:19 +0500

    adding comments

--- cypress/e2e/NFT copy.cy.ts
+++ cypress/e2e/NFT copy.cy.ts
@@ -5,8 +5,10 @@ describe('NFT on open sea api', () => {
         method: 'GET', url: 'https://staging-gateway.wrld.xyz/contracts', headers: { "Cookie": "token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjEsImlhdCI6MTY1Mjc5MDI1NCwiZXhwIjoxNjg0MzI2MjU0fQ.2vIFGELbH54q12-b1SIMwm0Eh7BLmdJMpPEX3E2XYy0" }, failOnStatusCode: false
       }).then(
         (response) => {
+
+        //This line will verify th inputs property i.e. internalType plus the value of internal type i.e. unit256
           expect(response.body.data[1].contractAbi[0].inputs[0]).to.have.property('internalType').contains('uint256')
-          //OR
+        //Whereas this line will only verify the value inside internalType
           expect(response.body.data[1].contractAbi[0].inputs[0].internalType).contains('uint256')
           expect(response.body.data[1].contractAbi[0].inputs[0]).to.have.keys('internalType', 'name', 'type')
           expect(response.body.data[1].contractAbi[0].inputs[0].name).contain('_mintSupplyCount')
