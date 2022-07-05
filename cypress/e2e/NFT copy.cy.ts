25747312e5c84f20b70326664e7fe3e59d890202
Chris Alex <mujahid.ali@wrld3d.com>
2022-07-05 15:29:39 +0500

    cpush1

--- cypress/e2e/NFT copy.cy.ts
+++ cypress/e2e/NFT copy.cy.ts
@@ -5,15 +5,16 @@ describe('NFT on open sea api', () => {
         method: 'GET', url: 'https://staging-gateway.wrld.xyz/contracts', headers: { "Cookie": "token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjEsImlhdCI6MTY1Mjc5MDI1NCwiZXhwIjoxNjg0MzI2MjU0fQ.2vIFGELbH54q12-b1SIMwm0Eh7BLmdJMpPEX3E2XYy0" }, failOnStatusCode: false
       }).then(
         (response) => {
-
         //This line will verify th inputs property i.e. internalType plus the value of internal type i.e. unit256
-          expect(response.body.data[1].contractAbi[0].inputs[0]).to.have.property('internalType').contains('uint256')
+          expect(response.body.data[0].contractAbi[0].inputs[0]).to.have.property("internalType")
+          expect(response.body.data[0].contractAbi[0]).to.have.property("stateMutability")
         //Whereas this line will only verify the value inside internalType
-          expect(response.body.data[1].contractAbi[0].inputs[0].internalType).contains('uint256')
-          expect(response.body.data[1].contractAbi[0].inputs[0]).to.have.keys('internalType', 'name', 'type')
-          expect(response.body.data[1].contractAbi[0].inputs[0].name).contain('_mintSupplyCount')
-          expect(response.body.data[1].contractAbi[0].inputs[0].type).contain('uint256')
-          expect(response.body.data[1].contractAbi[0].inputs[1].name).contain('_ownerMintReserveCount');
+          expect(response.body.data[1].contractAbi[1].inputs[0].internalType).contains('uint256')
+          expect(response.body.data[0].contractAbi[0].inputs[0]).to.have.keys('internalType', 'name', 'type')
+          expect(response.body.data[1].contractAbi[1].inputs[0].type).contain('uint256')
+          expect(response.body.data[0].contractAbi[0].inputs[0].name).contain('_mintSupplyLimit')
+          expect(response.body.data[0].contractAbi[1]).to.have.property('type').contain('error')
+          expect(response.body.data[0].contractAbi[1].name).contain('DroneMintSupplyReached');
           expect(response.status).to.eq(200);
         })
     });
